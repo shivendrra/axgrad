ce586bd378ca98cf36ca9b7969dcb3d8e381f893
Shivendra <shivharsh44@gmail.com>
2024-05-17 22:45:13 +0530

    fixed the issue in `concat()` function

--- axon/__init__.py
+++ axon/__init__.py
@@ -1,5 +1,3 @@
-from .modules.activations import LeakyRELU, relu, relu_derivative, LeakyRELU_derivative, tanh, tanh_derivative, sigmoid, sigmoid_derivative
 from .helpers.statics import ones, zeros, zeros_like, arange, randint, ns
-from .modules.nn import LayerNorm, Linear, MLP, Module, Sequence
 from .arrays import tensor
-from .tensor_ops import matmul, stack, cat
+from .tensor_ops import matmul, stack, concat
