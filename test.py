fixing grad broadcasting issue

test.py
@@ -1,4 +1,4 @@
-a = [[1, 4, 6], [1, 5, 8]]
+a = [[2, 3, 5], [6, 4, 9]]
 b = [[[1, 4, 6], [1, 5, 8]],
      [[1, 4, 6], [1, 5, 8]],
      [[1, 4, 6], [1, 5, 8]],
@@ -9,12 +9,26 @@ from axon import tensor
 a, b = tensor(a), tensor(b)
 
 c = a + b
+# for prev in c.prev:
+#   print(prev.grad)
 c.backward()
-print(c)
-print(c.grad)
-print(a.grad)
-print(b.grad)
+print("c: ", c.grad)
+print("a: ", a.grad)
+print("b: ", b.grad)
 
-a = a.broadcast(b)
-print(a)
-print(a.sum())
+# import torch
+
+# a, b = torch.tensor(a, requires_grad=True, dtype=torch.float64), torch.tensor(b, requires_grad=True, dtype=torch.float64)
+# c = a + b
+# d = c.sum()
+
+# c.retain_grad()
+# d.retain_grad()
+
+# d.backward()
+
+# print(d)
+# print("d grad: ", d.grad)
+# print("c grad: ", c.grad)
+# print("b grad: ", b.grad)
+# print("a grad: ", a.grad)