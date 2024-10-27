added layer&batch normalization & fixed autograd functions

axgrad/_tensor.py
@@ -298,7 +298,7 @@ class tensor:
     elif axis == 0:
       out = var_axis0(self.data)
     else:
-      mean_val = self.mean(axis=axis).data
+      mean_val = self.mean(axis=axis, keepdims=keepdims).data
       out = var_axis(self.data, mean_val, axis, ddof, keepdims)
     out = tensor(out, self.requires_grad, self.dtype)
     out.prev, out.grad_fn, out._backward = (self, ), "<VarBackwards>", Backward.var_backwards(out, self, axis, ddof, keepdims)

test.py
@@ -1,24 +1,5 @@
 import axgrad
 import axgrad.nn as nn
-
-# Example tensor
-t = axgrad.tensor([[[3, -4, 5], [1, 5, 6], [-1, 5, 0]], [[3, -4, 5], [1, 5, 6], [-1, 5, 0]]])
-
-# Create a norm instance
-l2_norm = axgrad.norm(t, p=2)
-# Access the computed norm value
-print(f"L2 Norm: {l2_norm}")
-
-# Create an L1 norm instance
-l1_norm = axgrad.norm(t, p=1)
-print(f"L1 Norm: {l1_norm}")
-
-# Create an L3 norm instance
-l3_norm = axgrad.norm(t, p=3)
-print(f"L3 Norm: {l3_norm}")
-
-import axgrad
-import axgrad.nn as nn
 from axgrad.nn import functional as F
 
 class MLP(nn.Module):
@@ -39,7 +20,7 @@ model = MLP(4, 10, 1)
 X = axgrad.tensor(axgrad.randn(shape=(4, 4)), requires_grad=True)  # Input tensor of shape (batch_size=4, features=4)
 Y = axgrad.tensor(axgrad.randn(shape=(4, 1)), requires_grad=True)  # Target tensor of shape (batch_size=4, 1)
 
-optimizer = nn.SGD(parameters=model.parameters(), lr=0.001)
+optimizer = nn.LARS(parameters=model.parameters(), lr=0.001)
 epoch = 10
 lr = 0.1
 
@@ -50,3 +31,22 @@ for n in range(epoch):
   loss.backward()
   optimizer.step()
   print(f"{n+1}th step, loss: {loss.data[0]:.6f}")
+
+# import axgrad
+# import axgrad.nn as nn
+
+# ln = nn.LayerNorm(normalized_shape=3, eps=1e-5)
+# x = axgrad.tensor([[1.9, -2.1, 3.3], [-4.0, -5.0, 5.0]])
+# y = axgrad.tensor([[1.0, -0.1, -3.9], [4.9, -0.1, 5.1]])
+
+# output = ln(x)
+# e = output * y
+# g = e.sum()
+# g.backward()
+
+# print(g.grad)
+# print(output.grad)
+# print(x.grad)
+
+# for p in ln.parameters():
+#   print(p.grad)