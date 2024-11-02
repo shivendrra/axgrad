a67ec8ad94a5527f26884b825266edc3ed017c93
Shivendra <shivharsh44@gmail.com>
2024-11-03 02:21:44 +0530

    added yinyang dataset generator, pretty simple code logic actually

---
+++ axgrad/utils/__init__.py
@@ -0,0 +1,2 @@
+from .tqdm import tqdm
+from .yinyang import RNG, yingyang_dataset
--- test.py
+++ test.py
@@ -1,17 +1,25 @@
+# import axgrad
+
+# t1 = axgrad.tensor([[1, 2, 3]], requires_grad=True)
+# t2 = axgrad.tensor([[4, 5, 6]], requires_grad=True)
+
+# stacked = axgrad.concat([t1, t2], axis=0)
+# d = stacked.tanh()
+# out = d.sum()
+# print(out)
+# print(stacked)
+
+# out.backward()
+# print(out.grad)
+# print(d.grad)
+# print(stacked.grad)
+# print(t1.grad)  # Should show gradients for t1
+# print(t2.grad)  # Should show gradients for t2
+
 import axgrad
-import axgrad.nn as nn
+from axgrad.utils import RNG, yingyang_dataset
 
-x = axgrad.tensor(axgrad.randn(shape=(2, 3)))
-y = axgrad.tensor(axgrad.randn(shape=(2, 3)))
-
-rms = nn.RMSNorm(dim=3)
-out = rms(x)
-e = out * y
-g = e.sum()
-g.backward()
-
-print(g.grad)
-print(out.grad)
-
-for p in rms.parameters():
-  print(p.grad)
+random = RNG(37)
+train_split, val_split, test_split = yingyang_dataset(random, n=20)
+train_split, val_split, test_split = axgrad.tensor(train_split), axgrad.tensor(val_split), axgrad.tensor(test_split)
+print(train_split, val_split, test_split)
