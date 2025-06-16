32457a913c44226aebed99bd41b8eae67cabfebe
Shivendra <shivharsh44@gmail.com>
2025-06-16 18:54:17 +0530

    added reshaping helper ops

---
+++ axgrad/csrc/cpu/shape_ops.h
@@ -0,0 +1,15 @@
+#ifndef __SHAPE_OPS__H__
+#define __SHAPE_OPS__H__
+
+#include <stdlib.h>
+
+extern "C" {
+  void reassign_tensor_ops(float* a, float* out, size_t size);
+  void equal_tensor_ops(float* a, float* b, float* out, size_t size);
+  void transpose_1d_tensor_ops(float* a, float* out, int* shape);
+  void transpose_2d_tensor_ops(float* a, float* out, int* shape);
+  void transpose_3d_tensor_ops(float* a, float* out, int* shape);
+  void transpose_ndim_tensor_ops(float* a, float* out, int* shape, int ndim);
+}
+
+#endif  //!__SHAPE_OPS__H__
