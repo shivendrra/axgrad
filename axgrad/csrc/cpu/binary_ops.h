6096afbdf41322dc389627fb2ef1ea1983b302dc
Shivendra <shivharsh44@gmail.com>
2025-06-16 19:06:42 +0530

    added `matmul` & `dot` product related ops

---
+++ axgrad/csrc/cpu/binary_ops.h
@@ -0,0 +1,14 @@
+#ifndef __BINARY_OPS__H__
+#define __BINARY_OPS__H__
+
+#include <stddef.h>
+
+extern "C" {
+  void matmul_tensor_ops(float* a, float* b, float* out, int* shape1, int* shape2);
+  void batch_matmul_tensor_ops(float* a, float* b, float* out, int* shape1, int* shape2, int* strides1, int* strides2);
+  void broadcasted_matmul_tensor_ops(float* a, float* b, float* out, int* shape1, int* shape2, int* strides1, int* strides2);
+  void dot_tensor_ops(float* a, float* b, float* out, size_t size);
+  void batch_dot_tensor_ops(float* a, float* b, float* out, size_t batch_count, size_t vector_size);
+}
+
+#endif  //!__BINARY_OPS__H__
