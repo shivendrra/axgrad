e6ad9b34a72910917ef7db382123ca7dc18a76f9
Shivendra <shivharsh44@gmail.com>
2025-06-26 00:47:14 +0530

    `cmake` build support added

--- axgrad/_core.py
+++ axgrad/_core.py
@@ -1,106 +1,81 @@
-"""
-  @core.py
-  @brief: main multi-dim tensor class to build tensor manipulation functions & ops
-  * tensor() & grad() is going to be built over on top of this, for proper grad management
-  * no backprop enabled
-"""
-
+import ctypes, os, sys, platform, sysconfig
+from ctypes import Structure, c_float, c_double, c_int, c_int8, c_int16, c_int32, c_int64, c_uint8, c_uint16, c_uint32, c_uint64, c_size_t, c_void_p, c_char_p, POINTER
 from typing import *
-from copy import deepcopy
 
-from ._dtype import Dtype
-from .utils.contiguous import ContiguousOps
-from .helpers.shape import get_shape, get_strides, get_size, transpose, flatten_recursive, unsqueeze, squeeze, swap_axes, reshape
-from .helpers.ops import broadcast, dot_product, determinant
-from .ops.main import *
-from .ops.binary import register_binary_operators
-from .ops.reduction import register_reduction_operators
-
-int8, int16, int32, int64, long = "int8", "int16", "int32", "int64", "long"
-float16, float32, float64, double = "float16", "float32", "float64", "double"
-
-class _tensor:
-  int8, int16, int32, int64, long, float16, float32, float64, double = int8, int16, int32, int64, long, float16, float32, float64, double
-  def __init__(self, data, dtype:Optional[Literal["int8", "int16", "int32", "int64", "float16", "float32", "float64", "long", "double"]]=None) -> None:
-    if data is not None and isinstance(data, list):
-      data = list(data)
-    self.dtype = _tensor.float32 if dtype is None else dtype
-    self.shape = get_shape(data)
-    self.data = Dtype.handle_conversion(data, self.dtype)
-    self.size = get_size(self.shape)
-    self.contiguous_ops = ContiguousOps(self) # creating an instance of coniguousops that works with this tensor
-    self.stride = get_strides(self.shape) # computing strides
-    self.is_scalar = True if self.size == (1) or self.size == (1,1) or self.size == (1,) else False
-
-  def __getitem__(self, index:tuple):
-    if isinstance(index, tuple):
-      data = self.data
-      for idx in index[:-1]:
-        data = data[idx]
-      return data[index[-1]]
-    else:
-      return self.data[index]
-
-  def __setattr__(self, name: str, value: Any) -> None:
-    super().__setattr__(name, value)
+def _get_lib_path():
+  pkg_dir = os.path.dirname(__file__)
+  possible_names = ['tensor', 'libtensor']
+  possible_exts = ['.pyd', '.dll', '.so', '.dylib', sysconfig.get_config_var('EXT_SUFFIX') or '']
+  search_dirs = [pkg_dir, os.path.join(pkg_dir, 'lib'), os.path.join(pkg_dir, '..', 'build')]
   
-  def __setitem__(self, index:tuple, value: Any) -> None:
-    if isinstance(index, tuple):
-      data = self.data
-      for idx in index[:-1]:
-        data = data[idx]
-      data[index[-1]] = value
-    else:
-      self.data[index] = value
+  for search_dir in search_dirs:
+    if not os.path.exists(search_dir): continue
+    for root, dirs, files in os.walk(search_dir):
+      for file in files:
+        for name in possible_names:
+          if file.startswith(name) and any(file.endswith(ext) for ext in possible_exts if ext):
+            return os.path.join(root, file)
+  
+  raise FileNotFoundError(f"Could not find tensor library in {search_dirs}. Available files: {[f for d in search_dirs if os.path.exists(d) for f in os.listdir(d)]}")
 
-  def __iter__(self) -> Iterator: return (item for item in self.data)
-  def __repr__(self) -> str: return f"{self.data}"
-  def __len__(self) -> int: return self.size
-  def is_contiguous(self) -> bool: return self.contiguous_ops.is_contiguous()
-  def make_contiguous(self) -> None: self.contiguous_ops.make_contiguous()
-  def compute_stride(self, shape: List[int]) -> List[int]: return self.contiguous_ops.compute_stride(shape)
-  def tolist(self) -> list: return list(self.data)
-  def astype(self, dtype: Dtype) -> "_tensor": return _tensor(self.data, dtype)
-  def copy(self) -> "_tensor": return _tensor(deepcopy(self.data), self.dtype)
-  def transpose(self) -> "_tensor": return _tensor(transpose(self.data), self.dtype)
-  def flatten(self, start_dim: int, end_dim: int) -> "_tensor": return _tensor(flatten_recursive(self.data, start_dim, end_dim), self.dtype)
-  def unsqueeze(self, dim: int=0) -> "_tensor": return _tensor(unsqueeze(self.data, dim), self.dtype)
-  def sequeeze(self, dim: int=0) -> "_tensor": return _tensor(squeeze(self.data, dim), self.dtype)
-  def reshape(self, new_shape: tuple) -> "_tensor": return _tensor(reshape(self.data, new_shape), self.dtype)
-  def __pow__(self, other) -> "_tensor": return _tensor(pow_tensor(self.data, other), self.dtype)
-  def sqrt(self, other) -> "_tensor": return _tensor(sqrt_tensor(self.data, other), self.dtype)
-  def rsqrt(self, other) -> "_tensor": return _tensor(rsqrt_tensor(self.data, other), self.dtype)
-  def dot(self, other: "_tensor") -> "_tensor": return _tensor(dot_product(self.data, other.data), self.dtype)
-  def det(self) -> "_tensor": return _tensor(determinant(self.data), self.dtype)
-  def exp(self) -> "_tensor": return _tensor(exp_tensor(self.data), self.dtype)
-  def log(self) -> "_tensor": return _tensor(log_tensor(self.data), self.dtype)
-  def ln(self) -> "_tensor": return _tensor(ln_tensor(self.data), self.dtype)
-  def sin(self) -> "_tensor": return _tensor(sin_tensor(self.data), self.dtype)
-  def sinh(self) -> "_tensor": return _tensor(sinh_tensor(self.data), self.dtype)
-  def cos(self) -> "_tensor": return _tensor(cos_tensor(self.data), self.dtype)
-  def cosh(self) -> "_tensor": return _tensor(cosh_tensor(self.data), self.dtype)
-  def tan(self) -> "_tensor": return _tensor(tan_tensor(self.data), self.dtype)
-  def tanh(self) -> "_tensor": return _tensor(tanh_tensor(self.data), self.dtype)
-  def relu(self) -> "_tensor": return _tensor(relu_tensor(self.data), self.dtype)
-  def gelu(self) -> "_tensor": return _tensor(gelu_tensor(self.data), self.dtype)
-  def sigmoid(self) -> "_tensor": return _tensor(sigmoid_tensor(self.data), self.dtype)
-  def leaky_relu(self) -> "_tensor": return _tensor(leaky_relu_tensor(self.data), self.dtype)
+lib = ctypes.CDLL(_get_lib_path())
 
-  def view(self, *new_shape:Union[int, list, tuple]):
-    if isinstance(new_shape[0], list) or isinstance(new_shape[0], tuple):
-      new_shape = tuple(new_shape[0])
-    elif isinstance(new_shape[0], int):
-      new_shape = tuple(new_shape)
-    self.make_contiguous()
-    flat_data = self.flatten()
-    total_elements = len(flat_data)
-    if total_elements != self.size:
-      raise ValueError("Total elements in new shape must match the number of elements in the original tensor")
-    out = _tensor(reshape(self.data, new_shape), dtype=self.dtype)
-    return out
+class DType:
+  FLOAT32, FLOAT64, INT8, INT16, INT32, INT64, UINT8, UINT16, UINT32, UINT64, BOOL = range(11)
 
-  def backward(self):
-    raise RuntimeError("backward() can only be called on tensors that require gradients. Use axgrad.tensor instead of _tensor.")
+class DTypeValue(ctypes.Union):
+  _fields_ = [("f32", c_float), ("f64", c_double), ("i8", c_int8), ("i16", c_int16), ("i32", c_int32), ("i64", c_int64), ("u8", c_uint8), ("u16", c_uint16), ("u32", c_uint32), ("u64", c_uint64), ("boolean", c_uint8)]
 
-register_binary_operators()
-register_reduction_operators()
+class CTensor(Structure):
+  _fields_ = [("data", c_void_p), ("strides", POINTER(c_int)), ("backstrides", POINTER(c_int)), ("shape", POINTER(c_int)), ("size", c_size_t), ("ndim", c_size_t), ("dtype", c_int), ("is_view", c_int)]
+
+def _setup_func(name, argtypes, restype):
+  func = getattr(lib, name)
+  func.argtypes, func.restype = argtypes, restype
+  return func
+
+_funcs = {
+  'create_tensor': ([POINTER(c_float), c_size_t, POINTER(c_int), c_size_t, c_int], POINTER(CTensor)),
+  'delete_tensor': ([POINTER(CTensor)], None), 'delete_data': ([POINTER(CTensor)], None),
+  'delete_shape': ([POINTER(CTensor)], None), 'delete_strides': ([POINTER(CTensor)], None),
+  'print_tensor': ([POINTER(CTensor)], None), 'out_data': ([POINTER(CTensor)], POINTER(c_float)),
+  'out_shape': ([POINTER(CTensor)], POINTER(c_int)), 'out_strides': ([POINTER(CTensor)], POINTER(c_int)),
+  'out_size': ([POINTER(CTensor)], c_int), 'contiguous_tensor': ([POINTER(CTensor)], POINTER(CTensor)),
+  'is_contiguous_tensor': ([POINTER(CTensor)], POINTER(CTensor)), 'make_contiguous_inplace_tensor': ([POINTER(CTensor)], POINTER(CTensor)),
+  'view_tensor': ([POINTER(CTensor)], POINTER(CTensor)), 'is_view_tensor': ([POINTER(CTensor)], POINTER(CTensor)),
+  'cast_tensor': ([POINTER(CTensor), c_int], POINTER(CTensor)), 'cast_tensor_simple': ([POINTER(CTensor), c_int], POINTER(CTensor)),
+  'get_dtype_size': ([c_int], c_size_t), 'get_dtype_name': ([c_int], c_char_p),
+  'dtype_to_float32': ([c_void_p, c_int, c_size_t], c_float), 'float32_to_dtype': ([c_float, c_void_p, c_int, c_size_t], None),
+  'convert_to_float32': ([c_void_p, c_int, c_size_t], POINTER(c_float)), 'convert_from_float32': ([POINTER(c_float), c_void_p, c_int, c_size_t], None),
+  'allocate_dtype_tensor': ([c_int, c_size_t], c_void_p), 'copy_with_dtype_conversion': ([c_void_p, c_int, c_void_p, c_int, c_size_t], None),
+  'cast_tensor_dtype': ([c_void_p, c_int, c_int, c_size_t], c_void_p), 'is_integer_dtype': ([c_int], c_int),
+  'is_float_dtype': ([c_int], c_int), 'is_unsigned_dtype': ([c_int], c_int), 'is_signed_dtype': ([c_int], c_int),
+  'clamp_to_int_range': ([c_double, c_int], c_int64), 'clamp_to_uint_range': ([c_double, c_int], c_uint64),
+  'get_dtype_priority': ([c_int], c_int), 'promote_dtypes': ([c_int, c_int], c_int),
+  'add_tensor': ([POINTER(CTensor), POINTER(CTensor)], POINTER(CTensor)), 'add_scalar_tensor': ([POINTER(CTensor), c_float], POINTER(CTensor)),
+  'add_broadcasted_tensor': ([POINTER(CTensor), POINTER(CTensor)], POINTER(CTensor)), 'sub_tensor': ([POINTER(CTensor), POINTER(CTensor)], POINTER(CTensor)),
+  'sub_scalar_tensor': ([POINTER(CTensor), c_float], POINTER(CTensor)), 'sub_broadcasted_tensor': ([POINTER(CTensor), POINTER(CTensor)], POINTER(CTensor)),
+  'mul_tensor': ([POINTER(CTensor), POINTER(CTensor)], POINTER(CTensor)), 'mul_scalar_tensor': ([POINTER(CTensor), c_float], POINTER(CTensor)),
+  'mul_broadcasted_tensor': ([POINTER(CTensor), POINTER(CTensor)], POINTER(CTensor)), 'div_tensor': ([POINTER(CTensor), POINTER(CTensor)], POINTER(CTensor)),
+  'div_scalar_tensor': ([POINTER(CTensor), c_float], POINTER(CTensor)), 'div_broadcasted_tensor': ([POINTER(CTensor), POINTER(CTensor)], POINTER(CTensor)),
+  'pow_tensor': ([POINTER(CTensor), c_float], POINTER(CTensor)), 'pow_scalar': ([c_float, POINTER(CTensor)], POINTER(CTensor)),
+  'log_tensor': ([POINTER(CTensor)], POINTER(CTensor)), 'exp_tensor': ([POINTER(CTensor)], POINTER(CTensor)),
+  'abs_tensor': ([POINTER(CTensor)], POINTER(CTensor)), 'matmul_tensor': ([POINTER(CTensor), POINTER(CTensor)], POINTER(CTensor)),
+  'batch_matmul_tensor': ([POINTER(CTensor), POINTER(CTensor)], POINTER(CTensor)), 'broadcasted_matmul_tensor': ([POINTER(CTensor), POINTER(CTensor)], POINTER(CTensor)),
+  'sin_tensor': ([POINTER(CTensor)], POINTER(CTensor)), 'sinh_tensor': ([POINTER(CTensor)], POINTER(CTensor)),
+  'cos_tensor': ([POINTER(CTensor)], POINTER(CTensor)), 'cosh_tensor': ([POINTER(CTensor)], POINTER(CTensor)),
+  'tan_tensor': ([POINTER(CTensor)], POINTER(CTensor)), 'tanh_tensor': ([POINTER(CTensor)], POINTER(CTensor)),
+  'transpose_tensor': ([POINTER(CTensor)], POINTER(CTensor)), 'equal_tensor': ([POINTER(CTensor), POINTER(CTensor)], POINTER(CTensor)),
+  'reshape_tensor': ([POINTER(CTensor), POINTER(c_int), c_int], POINTER(CTensor)), 'squeeze_tensor': ([POINTER(CTensor), c_int], POINTER(CTensor)),
+  'expand_dims_tensor': ([POINTER(CTensor), c_int], POINTER(CTensor)), 'flatten_tensor': ([POINTER(CTensor)], POINTER(CTensor)),
+  'sum_tensor': ([POINTER(CTensor), c_int, ctypes.c_bool], POINTER(CTensor)), 'min_tensor': ([POINTER(CTensor), c_int, ctypes.c_bool], POINTER(CTensor)),
+  'max_tensor': ([POINTER(CTensor), c_int, ctypes.c_bool], POINTER(CTensor)), 'mean_tensor': ([POINTER(CTensor), c_int, ctypes.c_bool], POINTER(CTensor)),
+  'var_tensor': ([POINTER(CTensor), c_int, c_int], POINTER(CTensor)), 'std_tensor': ([POINTER(CTensor), c_int, c_int], POINTER(CTensor)),
+  'zeros_like_tensor': ([POINTER(CTensor)], POINTER(CTensor)), 'ones_like_tensor': ([POINTER(CTensor)], POINTER(CTensor)),
+  'zeros_tensor': ([POINTER(c_int), c_size_t, c_size_t, c_int], POINTER(CTensor)), 'ones_tensor': ([POINTER(c_int), c_size_t, c_size_t, c_int], POINTER(CTensor)),
+  'randn_tensor': ([POINTER(c_int), c_size_t, c_size_t, c_int], POINTER(CTensor)), 'randint_tensor': ([c_int, c_int, POINTER(c_int), c_size_t, c_size_t, c_int], POINTER(CTensor)),
+  'uniform_tensor': ([c_int, c_int, POINTER(c_int), c_size_t, c_size_t, c_int], POINTER(CTensor)), 'fill_tensor': ([c_float, POINTER(c_int), c_size_t, c_size_t, c_int], POINTER(CTensor)),
+  'linspace_tensor': ([c_float, c_float, c_float, POINTER(c_int), c_size_t, c_size_t, c_int], POINTER(CTensor))
+}
+
+for name, (argtypes, restype) in _funcs.items(): _setup_func(name, argtypes, restype)
