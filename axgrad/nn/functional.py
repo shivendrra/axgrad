1e037659bd16c6349efe428fe4dc90fb332fb301
Shivendra <shivharsh44@gmail.com>
2025-07-01 18:46:02 +0530

    ``functional`` & ``loss`` class implemented
    Signed-off-by: Shivendra <shivharsh44@gmail.com>

---
+++ axgrad/nn/functional.py
@@ -0,0 +1,13 @@
+from .loss import MSELoss, MAELoss
+from ..tensor import Tensor
+from .module import Module
+
+def mse(pred: Tensor, target: Tensor, reduction: str = "mean") -> Tensor: return MSELoss(reduction)(pred, target)
+def mae(pred: Tensor, target: Tensor, reduction: str = "mean") -> Tensor: return MAELoss(reduction)(pred, target)
+
+class Tanh(Module):
+  def __init__(self, inplace: Tensor = False):
+    self.inplace = inplace
+    super().__init__()
+  def forward(self, x: Tensor) -> Tensor: return x.tanh()
+  def __repr__(self) -> str: return f"Tanh(inplace={self.inplace})"
