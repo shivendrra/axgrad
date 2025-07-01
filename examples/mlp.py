dc6ea833f4d16379d6c55c459f4b35cd43580160
Shivendra <shivharsh44@gmail.com>
2025-07-01 19:53:29 +0530

    tested MLP

--- examples/mlp.py
+++ examples/mlp.py
@@ -1,49 +1,49 @@
-import axgrad
 import axgrad.nn as nn
-from axgrad.nn.modules import linear as F
-import matplotlib.pyplot as plt
+from axgrad.utils import randn
+from axgrad.nn import functional as F
 
 class MLP(nn.Module):
   def __init__(self, _in, _hid, _out, bias=False) -> None:
     super().__init__()
     self.layer1 = nn.Linear(_in, _hid, bias)
-    self.gelu = nn.GELU()
+    self.gelu = nn.Tanh()
     self.layer2 = nn.Linear(_hid, _out, bias)
-  
+
   def forward(self, x):
     out = self.layer1(x)
     out = self.gelu(out)
     out = self.layer2(out)
     return out
 
-model = MLP(20, 30, 1)
 
-# Generate random input and target tensors
-X = axgrad.tensor(axgrad.randn(shape=(15, 20)), requires_grad=True)  # Input tensor of shape (batch_size=15, features=20)
-Y = axgrad.tensor(axgrad.randn(shape=(15, 1)), requires_grad=True)  # Target tensor of shape (batch_size=15, 1)
+model = MLP(20, 10, 10)
+optimizer = nn.SGD(parameters=model.parameters(), lr=1e-2)
+x = randn(10, 20)
+y = randn(10, 10)
 
-optimizer = nn.SGD(parameters=model.parameters(), lr=2e-5)
-epoch = 6000
+epoch = 50000
 losses = []
 steps = []
 
 for n in range(1, epoch + 1):
-  out = model.forward(X)
-  loss = F.mse(out, Y)
-  
+  out = model.forward(x)
+  loss = F.mae(out, y)
+
   optimizer.zero_grad()
   loss.backward()
   optimizer.step()
 
-  if n % 300 == 0:
-    losses.append(loss.data[0])  # Store the loss
-    steps.append(n)  # Store the step
-    print(f"{n}th step, loss: {loss.data[0]:.6f}")
+  if n % 5000 == 0:
+    losses.append(loss.tolist())
+    steps.append(n)
+    print(f"{n}th step, loss: {loss.tolist():.6f}")
 
-# Plotting the learning curve
+import matplotlib.pyplot as plt
+
+y_list = y.flatten().tolist()
+out_list = out.flatten().tolist()
+
 plt.figure(figsize=(12, 5))
-
-# Learning curve
 plt.subplot(1, 2, 1)
 plt.plot(steps, losses, marker='o', linestyle='-', color='b')
 plt.title('Learning Curve')
@@ -52,11 +52,9 @@ plt.ylabel('Loss')
 plt.grid()
 plt.xticks(steps)
 
-# Predictions vs Targets
 plt.subplot(1, 2, 2)
-# Scatter plot for actual values (Y) and predicted values (out)
-plt.scatter(range(len(Y.F.data)), Y.F.data, color='b', label='Actual Values')  # Actual values in blue
-plt.scatter(range(len(out.F.data)), out.F.data, color='r', label='Predicted Values')  # Predicted values in red
+plt.scatter(range(y.size), y_list, color='b', label='Actual Values')  # Actual values in blue
+plt.scatter(range(out.size), out_list, color='r', label='Predicted Values')  # Predicted values in red
 plt.title('Predictions vs Targets')
 plt.xlabel('Sample Index')
 plt.ylabel('Value')
--- test.py
+++ test.py
@@ -1,31 +1,13 @@
 import axgrad.nn as nn
 from axgrad.utils import randn
 
-class MLP(nn.Module):
-  def __init__(self, _in, _hid, _out, bias=False) -> None:
-    super().__init__()
-    self.layer1 = nn.Linear(_in, _hid, bias)
-    self.gelu = nn.Tanh()
-    self.layer2 = nn.Linear(_hid, _out, bias)
-
-  def forward(self, x):
-    out = self.layer1(x)
-    out = self.gelu(out)
-    out = self.layer2(out)
-    return out
-
-model = MLP(20, 30, 10)
-mse = nn.MSELoss()
-
 x = randn(10, 20)
 y = randn(10, 10)
-
-yp = model(x)
-loss = mse(yp, y)
+linear = nn.Linear(20, 10)
+yp = linear(x)
+loss = ((y - yp) ** 2 / x.size).sum()
 loss.backward()
 
-print("input grads: ", x.grad)
-print("input grads: ", y.grad)
-
-print("w1 grad: ", model.layer1.weight.grad)
-print("w2 grad: ", model.layer2.weight.grad)
+print(x.grad)
+print(y.grad)
+print(linear.weight.grad)
