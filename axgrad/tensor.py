055f988245cad292ee711a58a23c3aef72855035
Shivendra <shivharsh44@gmail.com>
2026-03-03 21:52:10 +0530

    fixed the initialization

--- axgrad/tensor.py
+++ axgrad/tensor.py
@@ -16,16 +16,48 @@ uint8, uint16, uint32, uint64 = "uint8", "uint16", "uint32", "uint64"
 boolean = "bool"
 
 class Tensor:
-  int8, int16, int32, int64, long, float32, float64, double, uint8, uint16, uint32, uint64, boolean = int8, int16, int32, int64, long, float32, float64, double, uint8, uint16, uint32, uint64, boolean  
-  def __init__(self, data: Union[List[Any], int, float], dtype: str=float32, requires_grad: bool=False):
-    if isinstance(data, CTensor): self.data, self.shape, self.size, self.ndim, self.strides, self.dtype = data, (), 0, 0, [], dtype or "float32"
-    elif isinstance(data, Tensor): self.data, self.shape, self.dtype, self.size, self.ndim, self.strides = data.data, data.shape, dtype or data.dtype, data.size, data.ndim, data.strides
+  int8, int16, int32, int64, long, float32, float64, double, uint8, uint16, uint32, uint64, boolean = int8, int16, int32, int64, long, float32, float64, double, uint8, uint16, uint32, uint64, boolean
+
+  def __init__(self, data, dtype: str=float32, requires_grad: bool=False):
+    if isinstance(data, CTensor):
+      self.data, self.shape, self.size, self.ndim, self.strides, self.dtype = data, (), 0, 0, [], dtype or "float32"
+    elif isinstance(data, Tensor):
+      self.data, self.shape, self.dtype, self.size, self.ndim, self.strides = data.data, data.shape, dtype or data.dtype, data.size, data.ndim, data.strides
     else:
-      data, shape = ShapeHelp.flatten([data] if isinstance(data, (int, float)) else data), tuple(ShapeHelp.get_shape(data))
-      self.size, self.ndim, self.dtype, self.shape, self.strides = len(data), len(shape), dtype or "float32", shape, ShapeHelp.get_strides(shape)
-      self._data_ctypes, self._shape_ctypes = (c_float * self.size)(*data.copy()), (c_int * self.ndim)(*shape)
+      raw = [data] if isinstance(data, (int, float)) else data
+      shape, flat = tuple(ShapeHelp.get_shape(raw)), ShapeHelp.flatten(raw)
+      self.size, self.ndim, self.dtype, self.shape, self.strides = len(flat), len(shape), dtype or "float32", shape, ShapeHelp.get_strides(shape)
+      self._data_ctypes = (c_float * self.size)(*flat)
+      self._shape_ctypes = (c_int * self.ndim)(*shape)
       self.data = lib.create_tensor(self._data_ctypes, c_size_t(self.ndim), self._shape_ctypes, c_size_t(self.size), c_int(DtypeHelp._parse_dtype(self.dtype)))
     self.requires_grad, self.hooks, self.grad_fn, self.grad = requires_grad, [], None, None
+
+  def tolist(self):
+    if self.size == 0: return []
+    ptr, flat = lib.out_data(self.data), [ptr[i] for i in range(self.size)]
+    if self.ndim == 0: return flat[0]
+    return ShapeHelp.reshape_list(flat, self.shape)
+
+  def astype(self, dtype: str):
+    parsed = DtypeHelp._parse_dtype(dtype)
+    out_ptr = lib.cast_tensor(self.data, c_int(parsed))
+    out = Tensor(out_ptr.contents, dtype=dtype, requires_grad=self.requires_grad)
+    out.shape, out.size, out.ndim, out.strides = self.shape, self.size, self.ndim, self.strides
+    return out
+
+  def __setattr__(self, name, value):
+    if name == "grad": [setattr(self, "_temp_value", hook(getattr(self, "_temp_value", value))) for hook in self.hooks]; value = getattr(self, "_temp_value", value)
+    super().__setattr__(name, value)
+
+  def register_hook(self, function): self.hooks.append(function)
+  def __str__(self): return (lib.print_tensor(self.data), "")[1]
+  def __hash__(self): return id(self)
+  def __getitem__(self, key): return _get_item_tensor(self, key)
+  def __setitem__(self, key, value): _set_item_tensor(self, key, value)
+  def __iter__(self): return _iter_item_tensor(self)
+  def contiguous(self): return contiguous_tensor_ops(self)
+  def make_contiguous(self): return make_contiguous_tensor_ops(self)
+  def view(self): return view_tensor_ops(self)
 
   def backward(self, gradient=None):
     assert self.ndim == 0 or (self.ndim == 1 and self.size == 1), "backward can only be called for scalar tensors"
@@ -44,80 +76,24 @@ class Tensor:
         for inp, grad in zip(tensor.grad_fn.input, grads):
           if isinstance(inp, Tensor) and inp.requires_grad: inp.grad = grad if inp.grad is None else inp.grad + grad
 
-  def __setattr__(self, name, value):
-    if name == "grad": [setattr(self, "_temp_value", hook(getattr(self, "_temp_value", value))) for hook in self.hooks]; value = getattr(self, "_temp_value", value)
-    super().__setattr__(name, value)
-
-  def register_hook(self, function): self.hooks.append(function)
-  def __str__(self): return (lib.print_tensor(self.data), "")[1]
-  def __hash__(self): return id(self)
-  def __getitem__(self, key): return _get_item_tensor(self, key)
-  def __setitem__(self, key, value): _set_item_tensor(self, key, value)
-  def __iter__(self): return _iter_item_tensor(self)
-  def contiguous(self): return contiguous_tensor_ops(self)
-  def make_contiguous(self): return make_contiguous_tensor_ops(self)
-  def view(self): return view_tensor_ops(self)
-
-  def astype(self, dtype: DType) -> "Tensor":
-    out = Tensor(lib.cast_tensor(self.data, c_int(DtypeHelp._parse_dtype(dtype))).contents, dtype=dtype, requires_grad=self.requires_grad)
-    out.shape, out.size, out.ndim, out.strides = self.shape, self.size, self.ndim, self.strides
-    return out
-
   def tolist(self):
     if self.size == 0: return []
-    if self.ndim == 0: 
-      indices_ctypes = (c_int * 1)(0)
-      return lib.get_item_tensor(self.data, indices_ctypes)
-    elif self.ndim == 1: return [self[i] for i in range(self.shape[0])]
-    else: return [self[i] for i in range(self.shape[0])]
+    if self.ndim == 0: return lib.get_item_tensor(self.data, (c_int * 1)(0))
+    return [self[i] for i in range(self.shape[0])]
 
-  def __eq__(self, other):
-    if isinstance(other, (int, float)):
-      out = Tensor(lib.equal_scalar(self.data, c_float(other)).contents, DType.BOOL, self.requires_grad)
+  def _cmp_op(self, other, scalar_fn, tensor_fn) -> "Tensor":
+    if isinstance(other, (int, float)): out = Tensor(scalar_fn(self.data, c_float(other)).contents, DType.BOOL, self.requires_grad)
     else:
       other = other if isinstance(other, Tensor) else Tensor(other, self.dtype, False)
-      out = Tensor(lib.equal_tensor(self.data, other.data).contents, DType.BOOL, self.requires_grad)
-    return (setattr(out, "grad", self.grad), setattr(out, "hooks", self.hooks), setattr(out, "grad_fn", self.grad_fn), out)[3] if self.requires_grad else out
+      out = Tensor(tensor_fn(self.data, other.data).contents, DType.BOOL, self.requires_grad)
+    return self._with_grad(out)
 
-  def __ne__(self, other):
-    if isinstance(other, (int, float)):
-      out = Tensor(lib.not_equal_scalar(self.data, c_float(other)).contents, DType.BOOL, self.requires_grad)
-    else:
-      other = other if isinstance(other, Tensor) else Tensor(other, self.dtype, False)
-      out = Tensor(lib.equal_tensor(self.data, other.data).contents, DType.BOOL, self.requires_grad)
-    return (setattr(out, "grad", self.grad), setattr(out, "hooks", self.hooks), setattr(out, "grad_fn", self.grad_fn), out)[3] if self.requires_grad else out
-
-  def __gt__(self, other):
-    if isinstance(other, (int, float)):
-      out = Tensor(lib.greater_scalar(self.data, c_float(other)).contents, DType.BOOL, self.requires_grad)
-    else:
-      other = other if isinstance(other, Tensor) else Tensor(other, self.dtype, False)
-      out = Tensor(lib.greater_tensor(self.data, other.data).contents, DType.BOOL, self.requires_grad)
-    return (setattr(out, "grad", self.grad), setattr(out, "hooks", self.hooks), setattr(out, "grad_fn", self.grad_fn), out)[3] if self.requires_grad else out
-
-  def __ge__(self, other):
-    if isinstance(other, (int, float)):
-      out = Tensor(lib.greater_equal_scalar(self.data, c_float(other)).contents, DType.BOOL, self.requires_grad)
-    else:
-      other = other if isinstance(other, Tensor) else Tensor(other, self.dtype, False)
-      out = Tensor(lib.greater_equal_tensor(self.data, other.data).contents, DType.BOOL, self.requires_grad)
-    return (setattr(out, "grad", self.grad), setattr(out, "hooks", self.hooks), setattr(out, "grad_fn", self.grad_fn), out)[3] if self.requires_grad else out
-
-  def __lt__(self, other):
-    if isinstance(other, (int, float)):
-      out = Tensor(lib.smaller_scalar(self.data, c_float(other)).contents, DType.BOOL, self.requires_grad)
-    else:
-      other = other if isinstance(other, Tensor) else Tensor(other, self.dtype, False)
-      out = Tensor(lib.smaller_tensor(self.data, other.data).contents, DType.BOOL, self.requires_grad)
-    return (setattr(out, "grad", self.grad), setattr(out, "hooks", self.hooks), setattr(out, "grad_fn", self.grad_fn), out)[3] if self.requires_grad else out
-
-  def __le__(self, other):
-    if isinstance(other, (int, float)):
-      out = Tensor(lib.smaller_equal_scalar(self.data, c_float(other)).contents, DType.BOOL, self.requires_grad)
-    else:
-      other = other if isinstance(other, Tensor) else Tensor(other, self.dtype, False)
-      out = Tensor(lib.smaller_equal_tensor(self.data, other.data).contents, DType.BOOL, self.requires_grad)
-    return (setattr(out, "grad", self.grad), setattr(out, "hooks", self.hooks), setattr(out, "grad_fn", self.grad_fn), out)[3] if self.requires_grad else out
+  def __eq__(self, other): return self._cmp_op(other, lib.equal_scalar, lib.equal_tensor)
+  def __ne__(self, other): return self._cmp_op(other, lib.not_equal_scalar, lib.not_equal_tensor)
+  def __gt__(self, other): return self._cmp_op(other, lib.greater_scalar, lib.greater_tensor)
+  def __ge__(self, other): return self._cmp_op(other, lib.greater_equal_scalar, lib.greater_equal_tensor)
+  def __lt__(self, other): return self._cmp_op(other, lib.smaller_scalar, lib.smaller_tensor)
+  def __le__(self, other): return self._cmp_op(other, lib.smaller_equal_scalar, lib.smaller_equal_tensor)
 
   def __add__(self, other): return add_tensor_ops(self, other)
   def __radd__(self, other): return radd_tensor_ops(self, other)
@@ -159,7 +135,5 @@ class Tensor:
   def transpose(self): return transpose_tensor_ops(self)
   def flatten(self): return flatten_tensor_ops(self)
   def reshape(self, new_shape: Union[Tuple, List]): return reshape_tensor_ops(self, new_shape)
-  # def clip(self, max_val: float): return clip_tensor_ops(self, max_val)
-  # def clamp(self, min_val: float,  max_val: float): return clamp_tensor_ops(self, min_val, max_val)
   def squeeze(self, axis: int): return squeeze_tensor_ops(self, axis)
   def unsqueeze(self, axis: int): return unsqueeze_tensor_ops(self, axis)
