8a2518aeeeba87c14a47aa5d4e6efd65c5a2b5b5
Shivendra <shivharsh44@gmail.com>
2026-03-03 22:00:30 +0530

    updating v0.1.0

--- axgrad/__init__.py
+++ axgrad/__init__.py
@@ -2,3 +2,6 @@ from .tensor import Tensor, int8, int16, int32, int64, long, float32, float64, d
 from .utils import zeros, ones, ones_like, zeros_like, randn, randint, uniform, fill, linspace, arange
 from . import nn
 from . import linalg
+
+__version__ = '0.1.0'
+__author__ = "Shivendra S"
